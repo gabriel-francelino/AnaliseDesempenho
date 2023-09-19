25ebcc5109a7142b29a7a74a888035b6ad62a6d9
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-09-19 11:09:55 -0300

    atualizando o código

--- simulacao.c
+++ simulacao.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 #include <float.h>
+#include <time.h>
+
+typedef struct {
+    double media_chegada;
+    double media_servico;
+    double tempo_simulacao;
+} parametros;
+
+void le_parametros(parametros * params){
+    printf("Informe o tempo medio entre clientes (s): ");
+    scanf("%lF", &params->media_chegada);
+    params->media_chegada = 1.0/params->media_chegada;
+
+    printf("Informe o tempo medio de servico (s): ");
+    scanf("%lF", &params->media_servico);
+    params->media_servico = 1.0/params->media_servico;
+
+    printf("Informe o tempo a ser simulado (s): ");
+    scanf("%lF", &params->tempo_simulacao);
+}
 
 double uniforme() {
 	double u = rand() / ((double) RAND_MAX + 1);
@@ -13,88 +32,69 @@ double uniforme() {
 }
 
 double min(double d1, double d2){
-    return (d1 < d2) ? d1 :  d2;
+    if(d1 < d2) return d1;
+    return d2;
 }
 
 int main(){
-    // iniciando a semente p/ a geracao dos numeros
-    //pseudo-aleatorios
     int semente = time(NULL);
-    // int semente = 10;
     srand(semente);
 
-    //Na Exponencial, E[X] = 1/l
-    // E[X] = 5
-    // 1/l = 5
-    // l = 1/5
-    // l = 0.2
-    double media_chegada;
-    printf("Informe o tempo medio de chegada entre clientes (s): ");
-    scanf("%lF", &media_chegada);
-    media_chegada = 1.0/media_chegada;
+    //le valores parametrizados
+    parametros params;
+    le_parametros(&params);
 
-    double media_servico;
-    printf("Informe o tempo medio de serviço (s): ");
-    scanf("%lF", &media_servico);
-    media_servico = 1.0/media_servico;
-
-    double tempo_simulacao;
-    printf("Informe o tempo a ser simulado (s): ");
-    scanf("%lF", &tempo_simulacao);
-    tempo_simulacao = 1.0/tempo_simulacao;
-
+    //variaveis de controle da simulacao
     double tempo_decorrido = 0.0;
-    double tempo_chegada = (-1.0/ media_chegada) * log(uniforme());
+    double tempo_chegada = (-1.0/params.media_chegada) * log(uniforme());
     double tempo_saida = DBL_MAX;
     unsigned long int fila = 0;
-    // printf("%lF\n", tempo_saida);
 
+    //variaveis de medidas de interesse
     double soma_ocupacao = 0.0;
 
-    while (tempo_decorrido < tempo_simulacao)
-    {
+    while(tempo_decorrido < params.tempo_simulacao){
         tempo_decorrido = min(tempo_chegada, tempo_saida);
-        // printf("tempo chegada: %lF\n", tempo_chegada);
-        // printf("tempo saida: %lF\n", tempo_saida);
-        // printf("tempo decorrido: %lF\n", tempo_decorrido);
-        // printf("boolean: %d\n", tempo_decorrido == tempo_chegada);
-        // getchar();
-        // getchar();
+        printf("%lF\n", tempo_decorrido);
 
-        if (tempo_decorrido == tempo_chegada)
-        {
-            // chegada
-            // a cabeça da fila é quem está em atendimento
-            if (!fila)
-            {
-                tempo_saida = tempo_decorrido + (-1.0/ media_servico) * log(uniforme());
+        if(tempo_decorrido == tempo_chegada){
+            //chegada
+            //a cabeca da fila eh quem esta em atendimento
+            if(!fila){
+                double tempo_servico =
+                    (-1.0/params.media_servico)
+                    * log(uniforme());
+                
+                tempo_saida = tempo_decorrido
+                    + tempo_servico;
+
+                soma_ocupacao += tempo_servico;
             }
-            fila++;     
-
-            tempo_chegada = tempo_decorrido + (-1.0/ media_chegada) * log(uniforme());      
-            
-        }
-        else if (tempo_decorrido == tempo_saida)
-        {
-            // saida
-            if (fila)
-            {
+            fila++;
+            tempo_chegada = tempo_decorrido + 
+            (-1.0/params.media_chegada) * log(uniforme());
+        } else if(tempo_decorrido == tempo_saida){
+            //saida
+            if(fila){
                 fila--;
-                tempo_saida = tempo_decorrido + (-1.0/ media_servico) * log(uniforme());
-            }
-            else
-            {
+                double tempo_servico =
+                    (-1.0/params.media_servico)
+                    * log(uniforme());
+                
+                tempo_saida = tempo_decorrido
+                    + tempo_servico;
+
+                soma_ocupacao += tempo_servico;
+            }else{
                 tempo_saida = DBL_MAX;
-            } 
-        }
-        else
-        {
-            printf("Evento inválido!\n");
+            }
+        } else{
+            printf("Evento invalido!\n");
             return(1);
-        }    
+        }
     }
-    
+    printf("ocupacao: %lF\n",
+        soma_ocupacao/tempo_decorrido);
 
-    // double valor = (-1.0/ l) * log(uniforme());
-
+    return 0;
 }
