7bf02ee5d207a92fbf77be8982d1f4316198eb0d
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-09-29 18:25:48 -0300

    comentando o código

--- simulacao.c
+++ simulacao.c
@@ -5,28 +5,32 @@
 #include <time.h>
 
 // Define a estrutura de dados para armazenar os parâmetros da simulação.
-typedef struct {
+typedef struct
+{
     double media_chegada;
     double media_servico;
     double tempo_simulacao;
 } parametros;
 
 // Define a estrutura de dados para calcular as métricas usando o método de Little.
-typedef struct {
-    unsigned long int no_eventos;
+typedef struct
+{
+    unsigned long int num_eventos;
     double tempo_anterior;
     double soma_areas;
 } little;
 
 // Inicializa uma estrutura "little" com valores iniciais.
-void inicia_little(little * l){
-    l->no_eventos = 0;
+void inicia_little(little *l)
+{
+    l->num_eventos = 0;
     l->tempo_anterior = 0.0;
     l->soma_areas = 0.0;
 }
 
 // Lê os parâmetros da simulação da entrada do usuário.
-void le_parametros(parametros * params){
+void le_parametros(parametros *params)
+{
     printf("Informe o tempo médio entre clientes (s): ");
     scanf("%lF", &params->media_chegada);
     params->media_chegada = 1.0 / params->media_chegada;
@@ -40,20 +44,24 @@ void le_parametros(parametros * params){
 }
 
 // Gera um número aleatório uniforme no intervalo (0, 1] usando a função rand.
-double uniforme() {
-    double u = rand() / ((double) RAND_MAX + 1);
+double uniforme()
+{
+    double u = rand() / ((double)RAND_MAX + 1);
     // Limitando entre (0,1]
     u = 1.0 - u;
     return u;
 }
 
 // Retorna o menor entre dois números.
-double min(double d1, double d2){
-    if(d1 < d2) return d1;
+double min(double d1, double d2)
+{
+    if (d1 < d2)
+        return d1;
     return d2;
 }
 
-int main(){
+int main()
+{
     int semente = time(NULL);
     srand(semente);
 
@@ -88,97 +96,105 @@ int main(){
      */
 
     // Loop principal da simulação.
-while(tempo_decorrido < params.tempo_simulacao){
+    while (tempo_decorrido < params.tempo_simulacao)
+    {
 
-    // Determina qual evento ocorre primeiro: a chegada de um cliente ou a saída de um cliente.
-    tempo_decorrido = min(tempo_chegada, tempo_saida);
+        // Determina qual evento ocorre primeiro: a chegada de um cliente ou a saída de um cliente.
+        tempo_decorrido = min(tempo_chegada, tempo_saida);
 
-    // Se um cliente chega, atualiza as variáveis de controle e medidas de interesse.
-    if(tempo_decorrido == tempo_chegada){
+        // Se um cliente chega, atualiza as variáveis de controle e medidas de interesse.
+        if (tempo_decorrido == tempo_chegada)
+        {
 
-        // Calcula o tempo de saída do cliente.
-        if(!fila){
-            double tempo_servico = (-1.0 / params.media_servico) * log(uniforme());
-            tempo_saida = tempo_decorrido + tempo_servico;
-            soma_ocupacao += tempo_servico;
+            // Calcula o tempo de saída do cliente se a fila estiver vazia.
+            if (!fila)
+            {
+                double tempo_servico = (-1.0 / params.media_servico) * log(uniforme());
+                tempo_saida = tempo_decorrido + tempo_servico;
+                soma_ocupacao += tempo_servico;
+            }
+
+            // Atualiza a variável de tamanho da fila.
+            fila++;
+
+            // Atualiza a variável de tamanho máximo da fila.
+            max_fila = fila > max_fila ? fila : max_fila;
+
+            // Atualiza a variável de tempo de chegada do próximo cliente.
+            tempo_chegada = tempo_decorrido + (-1.0 / params.media_chegada) * log(uniforme());
+
+            // Atualiza as áreas sob as curvas de E[N] e E[W] para o método de Little.
+            e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.num_eventos;
+            e_n.num_eventos++;
+            e_n.tempo_anterior = tempo_decorrido;
+
+            e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.num_eventos;
+            e_w_chegada.num_eventos++;
+            e_w_chegada.tempo_anterior = tempo_decorrido;
         }
+        // Se um cliente sai, atualiza as variáveis de controle e medidas de interesse.
+        else if (tempo_decorrido == tempo_saida)
+        {
 
-        // Atualiza a variável de tamanho da fila.
-        fila++;
+            // Atualiza a variável de tamanho da fila.
+            fila--;
 
-        // Atualiza a variável de tamanho máximo da fila.
-        max_fila = fila > max_fila ? fila : max_fila;
+            // Se ainda houver clientes na fila, atualiza a variável de tempo de saída do próximo cliente.
+            if (fila)
+            {
+                double tempo_servico = (-1.0 / params.media_servico) * log(uniforme());
+                tempo_saida = tempo_decorrido + tempo_servico;
+                soma_ocupacao += tempo_servico;
+            }
+            else
+            {
+                tempo_saida = DBL_MAX;
+            }
 
-        // Atualiza a variável de tempo de chegada do próximo cliente.
-        tempo_chegada = tempo_decorrido + (-1.0 / params.media_chegada) * log(uniforme());
+            // Atualiza as áreas sob as curvas de E[N] e E[W] para o método de Little.
+            e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.num_eventos;
+            e_n.num_eventos--;
+            e_n.tempo_anterior = tempo_decorrido;
 
-        // Atualiza as áreas sob as curvas de E[N] e E[W] para o método de Little.
-        e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
-        e_n.no_eventos++;
-        e_n.tempo_anterior = tempo_decorrido;
-
-        e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
-        e_w_chegada.no_eventos++;
-        e_w_chegada.tempo_anterior = tempo_decorrido;
-
-    // Se um cliente sai, atualiza as variáveis de controle e medidas de interesse.
-    } else if(tempo_decorrido == tempo_saida){
-
-        // Atualiza a variável de tamanho da fila.
-        fila--;
-
-        // Se ainda houver clientes na fila, atualiza a variável de tempo de saída do próximo cliente.
-        if(fila){
-            double tempo_servico = (-1.0 / params.media_servico) * log(uniforme());
-            tempo_saida = tempo_decorrido + tempo_servico;
-            soma_ocupacao += tempo_servico;
-        } else {
-            tempo_saida = DBL_MAX;
+            e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.num_eventos;
+            e_w_saida.num_eventos++;
+            e_w_saida.tempo_anterior = tempo_decorrido;
         }
-
-        // Atualiza as áreas sob as curvas de E[N] e E[W] para o método de Little.
-        e_n.soma_areas += (tempo_decorrido - e_n.tempo_anterior) * e_n.no_eventos;
-        e_n.no_eventos--;
-        e_n.tempo_anterior = tempo_decorrido;
-
-        e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
-        e_w_saida.no_eventos++;
-        e_w_saida.tempo_anterior = tempo_decorrido;
-
-    // Se um evento inválido ocorrer, imprime uma mensagem de erro e retorna um erro.
-    } else{
-        printf("Evento inválido!\n");
-        return(1);
+        // Se um evento inválido ocorrer, imprime uma mensagem de erro e retorna um erro.
+        else
+        {
+            printf("Evento inválido!\n");
+            return (1);
+        }
     }
-}
 
-// Atualiza as áreas para o cálculo final do método de Little.
-//
-// A área sob a curva de E[W] é calculada como a soma das áreas de todos os retângulos que representam o tempo de espera dos clientes.
-// Cada retângulo tem um comprimento igual ao tempo de espera do cliente e uma altura igual à taxa de chegada.
+    // Atualiza as áreas para o cálculo final do método de Little.
+    //
+    // A área sob a curva de E[W] é calculada como a soma das áreas de todos os retângulos que representam o tempo de espera dos clientes.
+    // Cada retângulo tem um comprimento igual ao tempo de espera do cliente e uma altura igual à taxa de chegada.
 
-e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.no_eventos;
-e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.no_eventos;
+    e_w_chegada.soma_areas += (tempo_decorrido - e_w_chegada.tempo_anterior) * e_w_chegada.num_eventos;
+    e_w_saida.soma_areas += (tempo_decorrido - e_w_saida.tempo_anterior) * e_w_saida.num_eventos;
 
-// Calcula e exibe as métricas finais.
-//
-// A ocupação média da fila é calculada como a soma do tempo de serviço de todos os clientes dividido pelo tempo total de simulação.
-// O tamanho máximo da fila é o maior número de clientes que já estiveram na fila ao mesmo tempo.
-//
-// O número médio de clientes na fila é calculado usando o método de Little. O método de Little afirma que o número médio de clientes na fila é igual à taxa de chegada vezes o tempo médio de espera.
-// O tempo médio de espera é calculado usando o método de Little. O método de Little afirma que o tempo médio de espera é igual à área sob a curva de E[W] dividida pela taxa de chegada.
-// O erro de Little é a diferença entre o número médio de clientes na fila calculado usando o método de Little e o número médio de clientes na fila calculado usando a fórmula tradicional.
+    // Calcula e exibe as métricas finais.
+    //
+    // A ocupação média da fila é calculada como a soma do tempo de serviço de todos os clientes dividido pelo tempo total de simulação.
+    // O tamanho máximo da fila é o maior número de clientes que já estiveram na fila ao mesmo tempo.
+    //
+    // O número médio de clientes na fila é calculado usando o método de Little. O método de Little afirma que o número médio de clientes na fila é igual à taxa de chegada vezes o tempo médio de espera.
+    // O tempo médio de espera é calculado usando o método de Little. O método de Little afirma que o tempo médio de espera é igual à área sob a curva de E[W] dividida pela taxa de chegada.
+    // O erro de Little é a diferença entre o número médio de clientes na fila calculado usando o método de Little e o número médio de clientes na fila calculado usando a fórmula tradicional.
 
-printf("Ocupação: %lF\n", (soma_ocupacao / tempo_decorrido));
-printf("Tamanho máximo da fila: %ld\n", max_fila);
+    printf("Ocupação: %lF\n", (soma_ocupacao / tempo_decorrido));
+    printf("Tamanho máximo da fila: %ld\n", max_fila);
 
-double e_n_calculo = e_n.soma_areas / tempo_decorrido;
-double e_w_calculo = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.no_eventos;
-double lambda = e_w_chegada.no_eventos / tempo_decorrido;
+    double e_n_calculo = e_n.soma_areas / tempo_decorrido;
+    double e_w_calculo = (e_w_chegada.soma_areas - e_w_saida.soma_areas) / e_w_chegada.num_eventos;
+    double lambda = e_w_chegada.num_eventos / tempo_decorrido;
 
-printf("E[N]: %lF\n", e_n_calculo);
-printf("E[W]: %lF\n", e_w_calculo);
-printf("Erro de Little: %.20lF\n", e_n_calculo - lambda * e_w_calculo);
+    printf("Número médio de clientes no sistema. E[N]: %lF\n", e_n_calculo);
+    printf("Tempo médio de espera de um cliente no sistema. E[W]: %lF\n", e_w_calculo);
+    printf("Erro de Little: %.20lF\n", e_n_calculo - lambda * e_w_calculo);
 
     return 0;
 }
